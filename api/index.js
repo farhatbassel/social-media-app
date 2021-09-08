f8555ee2459cdae127ae89032057bd58a794b2d0
farhatbassel <basfarhat96@gmail.com>
2021-09-08 09:30:12 +0200

    testing

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const path = require("path");
 
 dotenv.config();
@@ -50,6 +52,8 @@ app.post("/api/upload", upload.single("file"), (req, res) =>{
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 
 app.listen(8800, () => {
